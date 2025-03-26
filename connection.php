e38302740a4d531362e043bb9361b64bc79e1708
resetou <153208341+resetou@users.noreply.github.com>
2025-03-26 15:18:20 +0100

    testy8

--- connection.php
+++ connection.php
@@ -11,8 +11,9 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ];
 
-    // Créer une nouvelle instance de PDO
+    //1 Créer une nouvelle instance de PDO
     $pdo = new PDO($dsn, $username, $password, $options);
+    //4
     $sql = "SELECT COUNT(*) FROM utilisateurs WHERE pseudo = 'Gandalf'";
     $result = $pdo->query($sql);
 
@@ -20,21 +21,31 @@ try {
         die("Error execution request");
     }
     $result = $result->fetchColumn();
-    echo $result;
-
+    echo $result . "<br>";
+    //5.1
     $sql = "SELECT * from utilisateurs where pseudo = 'Gandalf'";
     $result = $pdo->query($sql);
     $user = $result->fetch(PDO::FETCH_NUM);
     echo "Statut Admin (indexé) : " . $user[3] . "<br>";
 
-    // 2. Récupérer en tableau associatif
+    // 5.2 Récupérer en tableau associatif
     $stmt = $pdo->query($sql); // Re-exécuter la requête
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     echo "Statut Admin (associatif) : " . $user['statutAdmin'] . "<br>";
-    // 3. Récupérer en objet anonyme
+    //5.3 Récupérer en objet anonyme
     $stmt = $pdo->query($sql);
     $user = $stmt->fetch(PDO::FETCH_OBJ);
     echo "Statut Admin (objet) : " . $user->statutAdmin . "<br>";
+
+    // 6 Recuperer les pseudos 
+    $sql = "SELECT pseudo from utilisateurs";
+    $result = $pdo->query($sql);
+    if ($result === false) {
+        die("Erreur lors de la récupération des pseudos.");
+    }
+    foreach ($result as $row) {
+        echo $row['pseudo'] . "</br>";
+    }
 } catch (PDOException $e) {
     // Gestion des erreurs de connexion
     echo "Erreur de connexion : " . $e->getMessage();
