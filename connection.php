4473ca33579facb4443498b643a49332b4d08871
resetou <153208341+resetou@users.noreply.github.com>
2025-03-26 15:10:18 +0100

    test7

--- connection.php
+++ connection.php
@@ -26,6 +26,15 @@ try {
     $result = $pdo->query($sql);
     $user = $result->fetch(PDO::FETCH_NUM);
     echo "Statut Admin (indexé) : " . $user[3] . "<br>";
+
+    // 2. Récupérer en tableau associatif
+    $stmt = $pdo->query($sql); // Re-exécuter la requête
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "Statut Admin (associatif) : " . $user['statutAdmin'] . "<br>";
+    // 3. Récupérer en objet anonyme
+    $stmt = $pdo->query($sql);
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+    echo "Statut Admin (objet) : " . $user->statutAdmin . "<br>";
 } catch (PDOException $e) {
     // Gestion des erreurs de connexion
     echo "Erreur de connexion : " . $e->getMessage();
