3d90cfb71b8f2d71ec357c8d895edaefab9b216f
Sodium Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2025-03-31 10:40:18 +0200

    workshop 2

--- dev-web/src/Controller/DefaultController.php
+++ dev-web/src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-include '/../Model/Model.php';
+include '/var/www/lebonplan/dev-web/src/Model/Model.php';
 
 class DefaultController extends AbstractController
 {
