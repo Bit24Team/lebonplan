9a4c0665133e01ce0af7951c4d5a1981427ba5d6
Sodium Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2025-04-01 11:12:00 +0200

    ajout de la recherche d'offres

--- dev-web/src/Controller/OfferController.php
+++ dev-web/src/Controller/OfferController.php
@@ -7,11 +7,13 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Model\OfferModel;
 
 class OfferController extends AbstractController
 {
+    //private OfferModel $model;
     #[Route('/offres', name: 'offers', methods: ['GET'])]
     public function show_offers(): Response
     {
@@ -28,4 +30,34 @@ class OfferController extends AbstractController
     {
         return new Response('Candidature envoyée !');
     }
+    #[Route('/ajouter/offre', name:'add_offer_page', methods: ['GET'])]
+    public function add_offer_page(): Response
+    {
+        return $this->render('add_offer.twig', ['is_active' => 'active']);
+    }
+    #[Route('/ajouter/offre', name:'add_offer', methods: ['POST'])]
+    public function add_offer(int $offer_id, Request $request, SessionInterface $session): Response
+    {
+        $manager_id = $session->get('user')['id'];
+        $title = $request->request->get("title");
+        $description = $request->request->get("description");
+        $location = $request->request->get("location");
+        $salary = $request->request->get("salary");
+        $contract_type = $request->request->get("contract_type");
+
+        // Assuming you have a model to handle the database operations
+        // $this->model->addOffer($company_id, $title, $description, $location, $salary, $contract_type);
+
+        return new Response('Offre ajoutée !');
+    }
+
+    #[Route('/api/offers', name: 'api_offers', methods: ['GET'])]
+    public function get_offer(): JsonResponse
+{
+    // Assuming you have a model to handle the database operations
+    // $offers = $this->model->getOffers();
+    return $this->json([
+        
+    ]);
+}
 }
