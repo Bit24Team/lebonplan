c49d0af5f46f19b68b4de074a93d8873f64bcd8a
Sodium Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2025-04-02 16:09:48 +0200

    feat: update render paths in DashboardController to use account templates

--- dev-web/src/Controller/dashboardController.php
+++ dev-web/src/Controller/dashboardController.php
@@ -30,7 +30,7 @@ class DashboardController extends AbstractController
         $charts = $this->model->getAdminChartsData();
         $recentActivity = $this->model->getRecentActivity();
 
-        return $this->render('admin.twig', [
+        return $this->render('account/admin.twig', [
             'stats' => $stats,
             'applications_chart' => $charts['applications_chart'],
             'users_chart' => $charts['users_chart'],
@@ -49,7 +49,7 @@ class DashboardController extends AbstractController
 
         $users = $this->model->getAllUsers();
 
-        return $this->render('admin.twig', [
+        return $this->render('account/admin.twig', [
             'active_tab' => 'users',
             'users' => $users
         ]);
@@ -65,7 +65,7 @@ class DashboardController extends AbstractController
 
         $companies = $this->model->getAllCompanies();
 
-        return $this->render('admin.twig', [
+        return $this->render('account/admin.twig', [
             'active_tab' => 'companies',
             'companies' => $companies
         ]);
@@ -82,7 +82,7 @@ class DashboardController extends AbstractController
         $filter = $request->query->get('filter', 'all');
         $offers = $this->model->getAllOffers($filter);
 
-        return $this->render('admin.twig', [
+        return $this->render('account/admin.twig', [
             'active_tab' => 'offers',
             'offers' => $offers,
             'filter' => $filter
@@ -100,7 +100,7 @@ class DashboardController extends AbstractController
         $filter = $request->query->get('filter', 'all');
         $applications = $this->model->getAllApplications($filter);
 
-        return $this->render('admin.twig', [
+        return $this->render('account/admin.twig', [
             'active_tab' => 'applications',
             'applications' => $applications,
             'filter' => $filter
@@ -117,7 +117,7 @@ class DashboardController extends AbstractController
 
         $settings = $this->model->getSiteSettings();
 
-        return $this->render('admin.twig', [
+        return $this->render('account/admin.twig', [
             'active_tab' => 'settings',
             'settings' => $settings
         ]);
@@ -151,7 +151,7 @@ class DashboardController extends AbstractController
 
         $data = $this->model->getManagerData($user['id']);
 
-        return $this->render('company.twig', [
+        return $this->render('account/company.twig', [
             'company' => $data['company'] ?? null,
             'offers' => $data['offers'] ?? [],
             'total_applications' => $data['total_applications'] ?? 0,
@@ -167,16 +167,16 @@ class DashboardController extends AbstractController
         if (!$userSession) {
             return $this->redirectToRoute('login_page');
         }
-    
+
         $data = $this->model->getCompleteUserData($userSession['id']);
-    
+
         if (empty($data)) {
             // Gérer le cas où l'utilisateur n'existe pas
             $session->remove('user');
             return $this->redirectToRoute('login_page');
         }
-    
-        return $this->render('user.twig', [
+
+        return $this->render('account/user.twig', [
             'user' => $data['user'],
             'user_skills' => $data['skills'],
             'applications' => $data['applications'],
