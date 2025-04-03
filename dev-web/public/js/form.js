87d1a12eb0073e36e769a7bb619f2ad3ef784e65
Zario44 <153208378+Zario44@users.noreply.github.com>
2025-04-04 00:19:23 +0200

    test js

--- dev-web/public/js/form.js
+++ dev-web/public/js/form.js
@@ -1,29 +1,43 @@
 const form = document.getElementById('formulaire');
 const cvFile = document.getElementById('filecv');
-const sizeMax = 2097152; //2Mo
-const afficher_cv = document.querySelector('.afficher_cv')
+const sizeMax = 2097152; // 2Mo
+const afficher_cv = document.querySelector('.afficher_cv');
 const cv_name = document.querySelector('.cv_name');
 const cvLabel = document.getElementById('cv');
 const lmFile = document.getElementById('filelm');
-const afficher_lm = document.querySelector('.afficher_lm')
+const afficher_lm = document.querySelector('.afficher_lm');
 const lmLabel = document.getElementById('lm');
 const lm_name = document.querySelector('.lm_name');
 const notifForm = document.querySelector('.notifForm');
 
-cvFile.addEventListener('change', function(){
-    const files = this.files;
-    if(files.length > 0){     
-        if(files[0].size > sizeMax){
+let ifPostuler = 0; // Déplacé en dehors de l’eventListener
+
+function error(valeur, text) {
+    const errorElem = valeur.nextElementSibling;
+    errorElem.classList.remove('invisible');
+    valeur.style.border = "1px solid red";
+    errorElem.innerText = text;
+}
+
+function success(valeur) {
+    const errorElem = valeur.nextElementSibling;
+    errorElem.classList.add('invisible');
+    valeur.style.border = "none";
+    errorElem.innerText = '';
+}
+
+cvFile.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+        if (file.size > sizeMax) {
             error(cvLabel, "Le fichier dépasse la taille maximale de 2Mo.");
             cvFile.value = "";
             afficher_cv.style.display = "none";
-        }
-        else if(!cvFile.classList.contains('invisible')){
+        } else {
             success(cvLabel);
-            const fileName = files[0].name;
+            cv_name.textContent = file.name;
             afficher_cv.style.display = "flex";
-            cv_name.innerHTML = fileName;
-        }   
+        }
     }
 });
 
@@ -32,7 +46,7 @@ lmFile.addEventListener('change', function () {
     if (file) {
         if (file.size > sizeMax) {
             error(lmLabel, "Le fichier dépasse la taille maximale de 2Mo.");
-            lmFile.value = ""; // Réinitialisation du champ file
+            lmFile.value = "";
             afficher_lm.style.display = "none";
         } else {
             success(lmLabel);
@@ -42,73 +56,51 @@ lmFile.addEventListener('change', function () {
     }
 });
 
-let ifError = 0;
-
-function error (valeur, text){
-    valeur.nextElementSibling.classList.remove('invisible');
-    valeur.style.border = "1px solid red";
-    valeur.nextElementSibling.innerText = text;
-    ifError = 1;
-}
-
-function success(valeur){
-    valeur.nextElementSibling.classList.add('invisible');
-    valeur.style.border = "none";
-    valeur.nextElementSibling.innerText = '';
-}
-
-form.addEventListener('submit', function(e){
-    ifError = 0;
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log("efezffs");
+    let ifError = 0;
 
-    const cvValue = cvFile.value;
-    const lmValue = lmFile.value;
-
-    if(cvValue === ""){
-        error(cvLabel, "CV obligatoire")
-    }
-    else if(!cv.classList.contains('invisible')){
+    if (!cvFile.value) {
+        error(cvLabel, "CV obligatoire");
+        ifError = 1;
+    } else {
         success(cvLabel);
     }
 
-    if(lmValue === ""){
-        error(lmLabel, "Lettre de motivation obligatoire")
+    if (!lmFile.value) {
+        error(lmLabel, "Lettre de motivation obligatoire");
+        ifError = 1;
+    } else {
+        success(lmLabel);
     }
-    else if(!lm.classList.contains('invisible')){
-        success(cvLabel);
-    }
 
-    const notifForm_O = `
-        <i class="fa-solid fa-check" style="color: #00ff00;"></i>
-        <span>Votre candidature à bien été transmise.</span>
-    `
+    if (ifError === 0) {
+        notifForm.classList.add("toast");
 
-    const notifForm_X = `
-        <i class="fa-solid fa-xmark" style="color: #ff0000;"></i>
-         <span>Vous ne pouvez pas postuler une nouvelle fois à cette annonce.</span>
-    `
-    let ifPostuler = 0;
-
-    if(ifError === 0){
-        notifForm.classList.add("toast")
-        if(ifPostuler === 0){
-            notifForm.innerHTML = notifForm_O;
-            ifPostuler += 1;
+        if (ifPostuler === 0) {
+            notifForm.innerHTML = `
+                <i class="fa-solid fa-check" style="color: #00ff00;"></i>
+                <span>Votre candidature a bien été transmise.</span>
+            `;
+            ifPostuler = 1;
+        } else {
+            notifForm.innerHTML = `
+                <i class="fa-solid fa-xmark" style="color: #ff0000;"></i>
+                <span>Vous ne pouvez pas postuler une nouvelle fois à cette annonce.</span>
+            `;
         }
-        else{
-            notifForm.innerHTML = notifForm_X;
-        }
-            setTimeout(function() {
-                notifForm.classList.add('fade-out');
-        
-                setTimeout(function(){
-                    notifForm.innerHTML='';
-                }, 500);
-            }, 2500);
-        console.log(ifPostuler);
+
+        setTimeout(function () {
+            notifForm.classList.add('fade-out');
+
+            setTimeout(function () {
+                notifForm.innerHTML = '';
+                notifForm.classList.remove('fade-out');
+            }, 500);
+        }, 2500);
     }
 });
+
 
 const btn_postuler = document.getElementById('btn_postuler');
 const formbg = document.querySelector('.formulairebg');
